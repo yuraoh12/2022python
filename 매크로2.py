fd3d384fbc929f4f47947ce15ed076a5871ed436
yuraoh123 <flamevert@naver.com>
2022-04-07 20:54:45 +0900

    시각화 3

--- 매크로2.py
+++ 매크로2.py
@@ -5,7 +5,11 @@ chrome_path = 'C:\오유라\chromedriver.exe'
 url = 'http://www.naver.com'
 browser = webdriver.Chrome(chrome_path)
 browser.get(url)
-
+searchbox = browser.find_element_by_xpath('//*[@id="query"]')
+searchbox.send_keys("코리아it학원")
+searchbox.send_keys(Keys.ENTER)
+searchbox.send_keys(Keys.ENTER)
+browser.find_element_by_xpath('//*[@id="web_1"]/div/div[]).close()
 #맨 처음 탭으로 이동
 #browser.switch_to.window(window_handles[0])
 #새롭게 열린 창, 팝업, 탭으로 전환
